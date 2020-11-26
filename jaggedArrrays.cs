65977b8dba499cc63f336195f0c99a33eeaba49f
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-26 16:54:16 +0200

    more explanations, corrections

--- jaggedArrrays.cs
+++ jaggedArrrays.cs
@@ -4,10 +4,10 @@ namespace _05.JaggedArrays
 {
     class jaggedArrrays  // array of arrays--to take less Ram memory, to save moemory, litte used in practice
     {
-        static void Main(string[] args)  //jaged arrayy -array on which riow the colums are different on different rows
+        static void Main(string[] args)  //jaged arrayy -array on which row the colums are different on different rows
         {
-            // int[][] jagged = new int[3][]; // jaggerd array / we say how many rows, but we left the colums empty-no numbers size
-            // jagged[0] = new int[3];   //on row tih index 0 we make here 3 colums
+            // int[][] jagged = new int[3][]; // jagged array / we say how many rows, but we left the colums empty-no numbers size
+            // jagged[0] = new int[3];   //on row with index 0 we make here 3 colums
             //jagged[1] = new int[2];  //on row with index 1  we make 2 colums
             //jagged[2] = new int[5]; // on row with index 2  we make 5 colums !!
 
