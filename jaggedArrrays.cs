4c5700b760a252843dd771f055d2f5c869cb95f1
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-26 16:51:57 +0200

    some explanations made more to jagged arrays

--- jaggedArrrays.cs
+++ jaggedArrrays.cs
@@ -6,10 +6,10 @@ namespace _05.JaggedArrays
     {
         static void Main(string[] args)  //jaged arrayy -array on which riow the colums are different on different rows
         {
-           // int[][] jagged = new int[3][]; // jaggerd array / we say how many rows, but we left the colums empty-no numbers size
-           // jagged[0] = new int[3];
-            //jagged[1] = new int[2];
-            //jagged[3] = new int[5];it
+            // int[][] jagged = new int[3][]; // jaggerd array / we say how many rows, but we left the colums empty-no numbers size
+            // jagged[0] = new int[3];   //on row tih index 0 we make here 3 colums
+            //jagged[1] = new int[2];  //on row with index 1  we make 2 colums
+            //jagged[2] = new int[5]; // on row with index 2  we make 5 colums !!
 
             int[][] jagged = new int[5][];
             for (int i = 1; i < jagged.Length; i++)
